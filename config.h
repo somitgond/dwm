04c5c4fc4c049b18a03b06aa898947e427165d50
somit gond <somitgond2@gmail.com>
2024-08-15 21:34:16 +0530

    remove patch titlecolor

--- config.h
+++ config.h
@@ -4,16 +4,16 @@
 /* appearance */
 static const unsigned int borderpx       = 2;        /* border pixel of windows */
 static const unsigned int snap           = 16;       /* snap pixel */
-static const unsigned int gappx          = 5;             /* gaps between windows */
+static const unsigned int gappx          = 3;             /* gaps between windows */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Meslo LG M DZ:style=Bold:size=11:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Meslo LG M DZ:style=Bold:size=11:antialias=true:autohint=true";
+static const char *fonts[]          = { "MesloLGMDZ Nerd Font Mono:style=Bold:size=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "MesloLGMDZ Nerd Font Mono:style=Bold:size=11:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#a2a2a2";  // bbbbbb
@@ -21,16 +21,18 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577"; 
 static const char col_gray[]        = "#929292"; //96b6c5 98971a blue gray: 98AFC7
 static const char col_black[]       = "#282828";  // Gunmetal color 16161D
+static const char col_orange[]      = "#d79921";  
+static const char col_greendim[]   = "#689d6a";  
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_black, col_black },
-	[SchemeSel]  = { col_black, col_gray,  col_gray },
-	[SchemeTitle]  = { col_gray3, col_black, col_black }, // window title color scheme
+	[SchemeNorm] = { col_gray3, col_black, col_black},
+	[SchemeSel]  = { col_black, col_gray,  col_greendim},
 };
 
 /* tagging  Japense/chinese characters*/
-static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I" };
 
 static const Rule rules[] = {
 	/* xprop(1):
--- dwm.c
+++ dwm.c
@@ -73,7 +73,7 @@
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel, SchemeTitle }; /* color schemes */
+enum { SchemeNorm, SchemeSel }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetSystemTray, NetSystemTrayOP, NetSystemTrayOrientation, NetSystemTrayOrientationHorz,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
