9ef3eb86b84a47fd9b371ac3978039ca6e573498
somit <somitgond2@gmail.com>
2026-02-13 09:11:23 +0530

    updated gruvbox

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -12,24 +12,43 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char *fonts[]          = { "JetBrains Mono:style=Bold:size=11" };
+static const char dmenufont[]       = "JetBrains Mono:style=Bold:size=11";
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#005577";
+// static const char *colors[][3]      = {
+// 	/*               fg         bg         border   */
+// 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+// 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+// };
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#282828";
+static const char col_gray3[]       = "#3f3f3f";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_fg[]          = "#ddc7a1";
+static const char col_magenta[]     = "#d3869b";
+static const char col_cyan[]         = "#ea6962";
+static const char col_green[]       = "#a9b665";
+static const char col_blue[]        = "#7daea3";
+static const char col_yellow[]      = "#d8a657";
+static const char col_aqua[]        = "#89b482";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	//                      fg            bg              border
+	[SchemeNorm] =        { col_fg,       col_gray1,   col_gray3 },
+	[SchemeSel]  =        { col_gray1,    col_aqua,    col_aqua },
 };
+
+
 
 static const char *const autostart[] = {
   "slstatus", NULL,
 	"xclip", NULL,
   "dunst", NULL,
   "unclutter", NULL,
+  "picom --backend xrender -b", NULL,
   "setxkbmap", "-option", "caps:escape", NULL,
 	NULL /* terminate */
 };
@@ -81,8 +100,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_fg, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
@@ -94,12 +113,12 @@ static const char *brighter[] = { "brightnessctl", "set", "5%+", NULL };
 static const char *dimmer[]   = { "brightnessctl", "set", "4%-", NULL };
 static const char *networkManagerOff[]   = { "nmcli", "networking", "off", NULL };
 
-static const char *termcmd2[] = { "st", NULL };
+static const char *termcmd2[] = { "alacritty", NULL };
 static const char *browsercmd[] = {"firefox", NULL};
 
 Autostarttag autostarttaglist[] = {
 	{.cmd = termcmd2, .tags = 1 << 0 },
-	{.cmd = browsercmd, .tags = 1 << 1 },
+	// {.cmd = browsercmd, .tags = 1 << 1 },
 	{.cmd = NULL, .tags = 0 },
 };
 
